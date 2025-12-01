9b0e85e7dd5a3e9dce825b8f1902b55f337bfd4f
KanielDasper <dkdanielkasper@gmail.com>
2025-12-01 19:43:11 +0100

    day1 solution

--- 1dec.py
+++ 1dec.py
@@ -1,36 +1,71 @@
 import csv
 import re
 
-with open("data/dial_input.csv") as f:
+with open("data/day1_dial_input.csv") as f:
     reader = csv.reader(f)
     dial_inputs = [row[0] for row in reader]
 
 
-def convert_to_number(item) -> int:
+def convert_item_to_number(item) -> int:
     _, number = re.match(r"([A-Za-z]+)(\d+)", item).groups()
     return int(number)
 
 
-def input_parser(input_list) -> int:
-    dial_start_number = 50
-    no_of_times_passed_0 = 0
+def single_click_parser(input_list) -> int:
+    dial_number = 50
+    password = 0
 
     for item in input_list:
+        number = convert_item_to_number(item)
+
         if item.startswith("L"):
-            number = convert_to_number(item)
-            dial_start_number -= number
+            dial_number -= number
+        elif item.startswith("R"):
+            dial_number += number
 
-        if item.startswith("R"):
-            number = convert_to_number(item)
-            dial_start_number += number
+        while dial_number > 99 or dial_number < 0:
+            dial_number = dial_number % 100
 
-        if dial_start_number > 99 or dial_start_number < 0:
-            dial_start_number = dial_start_number % 100
+        if dial_number == 0:
+            password += 1
 
-        if dial_start_number == 0:
-            no_of_times_passed_0 += 1
-
-    return no_of_times_passed_0
+    return password
 
 
-print(input_parser(dial_inputs))
+first_password = single_click_parser(dial_inputs)
+print("First password:", first_password)
+
+
+def multiple_click_parser(input_list) -> int:
+    dial_number = 50
+    password = 0
+    dial_loc = 0
+
+    for item in input_list:
+        number = convert_item_to_number(item)
+
+        if item.startswith("L"):
+            dial_loc = -number
+        elif item.startswith("R"):
+            dial_loc = number
+
+        prev_pointer = dial_number
+        dial_number += dial_loc
+
+        crossings = abs(dial_number // 100)
+        dial_number = dial_number % 100
+
+        if dial_number == 0 and dial_loc > 0:
+            crossings -= 1
+        if prev_pointer == 0 and dial_loc < 0:
+            crossings -= 1
+        if dial_number == 0:
+            crossings += 1
+
+        password += crossings
+
+    return password
+
+
+second_password = multiple_click_parser(dial_inputs)
+print("Second password:", second_password)
